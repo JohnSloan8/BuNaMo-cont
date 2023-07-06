ba258d024b1b1f825086d263e98213f3be13d875
JohnSloan8 <johnsloan1984@gmail.com>
2023-07-06 07:38:18 +0100

    space

--- convert-XML-to-JSON-scripts/convertAdjectives.py
+++ convert-XML-to-JSON-scripts/convertAdjectives.py
@@ -5,6 +5,7 @@ import json
 
 adjective_XML_filenames = os.listdir('../BuNaMo/adjective')
 
+
 def main():
 
     default_adjective_objects = {}
@@ -13,7 +14,8 @@ def main():
     count = 1
     for fname in adjective_XML_filenames:
         print(f"{count}: {fname}\n")
-        object_from_one_file = parse_one_xml_file('../BuNaMo/adjective/' + fname)
+        object_from_one_file = parse_one_xml_file(
+            '../BuNaMo/adjective/' + fname)
         default = object_from_one_file[0].pop('default')
         default_adjective_objects[default] = object_from_one_file[0]
         adjective_variations_objects.extend(object_from_one_file[1])
@@ -25,13 +27,14 @@ def main():
     with open('../converted-JSON-data/adjectives/adjectives.json', 'w') as outfile:
         json.dump(adjective_variations_objects, outfile, ensure_ascii=False)
 
+
 def parse_one_xml_file(filename):
     mytree = ET.parse(filename)
     root = mytree.getroot()
 
     default_json_object = copy.deepcopy(root.attrib)
     json_variations = []
-    for el in root:       
+    for el in root:
         el_json = {'default': default_json_object['default']}
         for item in el.items():
             if item[0] == 'default':
@@ -54,6 +57,7 @@ def parse_one_xml_file(filename):
     # print('default_json_object', default_json_object)
     # print('json_variations', json_variations)
     return [default_json_object, json_variations]
+
 
 if __name__ == "__main__":
     main()
